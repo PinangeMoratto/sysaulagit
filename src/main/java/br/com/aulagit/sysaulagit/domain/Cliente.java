04b03e575052edb52e7076ee29e094b514092463
Samuel Moratto <ti.samuel.senac@hotmail.com>
2023-03-09 19:09:41 -0300

    "Criação da Classe Cliente"

---
+++ src/main/java/br/com/aulagit/sysaulagit/domain/Cliente.java
@@ -0,0 +1,50 @@
+package br.com.aulagit.sysaulagit.domain;
+
+public class Cliente {
+    private String nome;
+    private String idade;
+    private String endereco;
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return String return the idade
+     */
+    public String getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return String return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+}
